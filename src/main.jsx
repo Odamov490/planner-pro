60cb6ee2c7e28adb052cd5b7f637065c41cddcc2
Odamov490 <piskentems@gmail.com>
2026-03-26 15:59:44 +0500

    Yangilandi

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import {TaskProvider} from "./context/TaskContext";
-import {requestPermission} from "./utils/notify";
 
+import { TaskProvider } from "./context/TaskContext";
+import { NotificationProvider } from "./context/NotificationContext"; // 🔥 QO‘SHILDI
+import { requestPermission } from "./utils/notify";
+
+// 🔔 notification permission
 requestPermission();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
- <TaskProvider><App/></TaskProvider>
+  <TaskProvider>
+    <NotificationProvider>
+      <App />
+    </NotificationProvider>
+  </TaskProvider>
 );
